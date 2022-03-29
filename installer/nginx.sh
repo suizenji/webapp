96cba1c27bdbe78ebcec24afa7099d45d5509b06
suizenji <suizenji@pop-os.localdomain>
2022-03-29 21:07:53 +0900

    config

--- installer/nginx.sh
+++ installer/nginx.sh
@@ -12,3 +12,5 @@ openssl genrsa -out $SSL_KEY 2>/dev/null
 openssl req -new -x509 -key $SSL_KEY -batch -out $SSL_CRT
 
 systemctl restart nginx
+
+[[ "$(getenforce)" == "Disabled" ]] || { sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config; setenforce 0; }
