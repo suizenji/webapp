2a9b1d6b130af77591a98badbb7bf87a62cf2b1f
suizenji <suizenji@pop-os.localdomain>
2022-04-29 12:11:41 +0900

    rename:user to mamber

--- app/src/Entity/MEMBER.php
+++ app/src/Entity/MEMBER.php
@@ -2,14 +2,13 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
+use App\Repository\MEMBERRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-#[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`user`')]
-class User implements UserInterface, PasswordAuthenticatedUserInterface
+#[ORM\Entity(repositoryClass: MEMBERRepository::class)]
+class MEMBER implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
