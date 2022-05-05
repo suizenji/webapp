758863c6894f9e8c49ca11e97047eddead205e6b
suizenji <suizenji@pop-os.localdomain>
2022-05-05 15:40:38 +0900

    form submit

--- app/src/Controller/EnrollController.php
+++ app/src/Controller/EnrollController.php
@@ -2,18 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\MEMBER;
 use App\Form\EnrollType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/enroll', name: 'enroll_')]
+#[Route('/enroll', name: 'enroll_', methods: ['HEAD', 'GET'])]
 class EnrollController extends AbstractController
 {
-    #[Route('/input', name: 'input', methods: ['GET'])]
-    public function input(): Response
+    #[Route('/input', name: 'input', methods: ['POST'])]
+    public function input(Request $request): Response
     {
-        $form = $this->createForm(EnrollType::class);
+        $member = new MEMBER();
+        $form = $this->createForm(EnrollType::class, $member);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            var_dump('submit!');
+        }
 
         return $this->render('enroll/input.html.twig', [
             'form' => $form->createView(),
--- app/src/Form/EnrollType.php
+++ app/src/Form/EnrollType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\MEMBER;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +14,8 @@ class EnrollType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
             ->add('password')
+            ->add('submit', SubmitType::class)
         ;
     }
 
