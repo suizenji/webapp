82ae39157f5ac0e631f4d56e689bfb1f9930b6d2
suizenji <suizenji@pop-os.localdomain>
2022-05-05 15:41:25 +0900

    fix-comment

--- app/src/Security/Authenticator.php
+++ app/src/Security/Authenticator.php
@@ -50,7 +50,7 @@ class Authenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
