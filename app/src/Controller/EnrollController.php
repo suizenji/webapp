6ec5b95d133bb5697127dd19285ce370aa77f86d
suizenji <suizenji@pop-os.localdomain>
2022-05-05 15:59:25 +0900

    store form

--- app/src/Controller/EnrollController.php
+++ app/src/Controller/EnrollController.php
@@ -4,14 +4,28 @@ namespace App\Controller;
 
 use App\Entity\MEMBER;
 use App\Form\EnrollType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @see https://symfony.com/doc/current/doctrine.html
+ * @see https://symfony.com/doc/current/the-fast-track/ja/14-form.html
+ * @see https://symfony.com/doc/current/forms.html
+ * @see https://symfony.com/doc/current/form/form_customization.html
+ */
 #[Route('/enroll', name: 'enroll_', methods: ['HEAD', 'GET'])]
 class EnrollController extends AbstractController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     #[Route('/input', name: 'input', methods: ['POST'])]
     public function input(Request $request): Response
     {
@@ -21,6 +35,9 @@ class EnrollController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             var_dump('submit!');
+
+            $this->em->persist($member);
+            $this->em->flush();
         }
 
         return $this->render('enroll/input.html.twig', [
