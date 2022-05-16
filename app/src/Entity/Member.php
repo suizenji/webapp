15bb43077606b7777f2ff0e66df2ca437264617a
suizenji <suizenji@pop-os.localdomain>
2022-05-17 07:44:28 +0900

    make:registration-form

--- app/src/Entity/Member.php
+++ app/src/Entity/Member.php
@@ -4,11 +4,13 @@ namespace App\Entity;
 
 use App\Repository\MemberRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: MemberRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class Member implements UserInterface, PasswordAuthenticatedUserInterface
 {
     public const EMAIL_LENGTH_MAX = 180;
