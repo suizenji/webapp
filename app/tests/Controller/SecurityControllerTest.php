1ae72139bf18d305b30e93fae6525f4260efd118
suizenji <suizenji@pop-os.localdomain>
2022-05-20 12:07:22 +0900

    form test

--- app/tests/Controller/SecurityControllerTest.php
+++ app/tests/Controller/SecurityControllerTest.php
@@ -13,21 +13,32 @@ class SecurityControllerTest extends WebTestCase
         $client = static::createClient();
         $crawler = $client->request('GET', '/login');
 
-        $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('#login', 'sign up');
     }
 
     public function testVisitingWhileLoggedIn(): void
     {
         $client = static::createClient();
-
         $memberRepository = static::getContainer()->get(MemberRepository::class);
         $testUser = $memberRepository->findOneByEmail(MemberFixtures::EMAIL);
 
         $client->loginUser($testUser);
-
         $crawler = $client->request('GET', '/login');
-        $this->assertResponseIsSuccessful();
+
+        $this->assertSelectorTextContains('#logout', 'Logout');
+    }
+
+    public function testLoginForm(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+
+        $crawler = $client->submitForm('Sign in', [
+            'email' => MemberFixtures::EMAIL,
+            'password' => MemberFixtures::PW,
+        ]);
+
+        $crawler = $client->followRedirect();
         $this->assertSelectorTextContains('#logout', 'Logout');
     }
 }
