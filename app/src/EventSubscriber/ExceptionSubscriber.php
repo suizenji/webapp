bb6c511c430152af97a3b56af08f9e5612d4f593
suizenji <suizenji@pop-os.localdomain>
2022-05-22 10:57:39 +0900

    exception subscriber

---
+++ app/src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * @link https://symfony.com/doc/current/event_dispatcher.html
+ */
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        // return the subscribed events, their methods and priorities
+        return [
+            KernelEvents::EXCEPTION => [
+                ['processException', 10],
+                ['logException', 0],
+                ['notifyException', -10],
+            ],
+        ];
+    }
+
+    public function processException(ExceptionEvent $event)
+    {
+        var_dump(__FUNCTION__);
+    }
+
+    public function logException(ExceptionEvent $event)
+    {
+        var_dump(__FUNCTION__);
+    }
+
+    public function notifyException(ExceptionEvent $event)
+    {
+        var_dump(__FUNCTION__);
+    }
+}
