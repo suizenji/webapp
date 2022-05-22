2302ef99eda3df5944d92782ad59555859324d15
suizenji <suizenji@pop-os.localdomain>
2022-05-23 08:55:15 +0900

    Exception subscriber renders view.

--- app/src/EventSubscriber/ExceptionSubscriber.php
+++ app/src/EventSubscriber/ExceptionSubscriber.php
@@ -3,14 +3,22 @@
 namespace App\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Twig\Environment;
 
 /**
  * @see https://symfony.com/doc/current/event_dispatcher.html
  */
 class ExceptionSubscriber implements EventSubscriberInterface
 {
+    const PATH_PAGES_ERROR = 'bundles/TwigBundle/Exception/';
+
+    public function __construct(private Environment $twig)
+    {
+    }
+
     public static function getSubscribedEvents()
     {
         // return the subscribed events, their methods and priorities
@@ -35,6 +43,12 @@ class ExceptionSubscriber implements EventSubscriberInterface
 
     public function notifyException(ExceptionEvent $event)
     {
-        var_dump(__FUNCTION__);
+        $template = self::PATH_PAGES_ERROR . 'error.html.twig';
+        $content = $this->twig->render($template);
+
+        $response = new Response();
+        $response->setContent($content);
+
+        $event->setResponse($response);
     }
 }
