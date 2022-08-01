e1512a8989ae43cd99c2ef4884311732238e6c7c
suizenji <suizenji@pop-os.localdomain>
2022-08-01 13:01:16 +0900

    post crud

---
+++ app/src/Form/PostType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('editor')
+            ->add('public')
+            ->add('title')
+            ->add('content')
+            ->add('created_at')
+            ->add('updated_at')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
