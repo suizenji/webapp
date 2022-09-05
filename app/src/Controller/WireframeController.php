73cd22d813898737bd8ce1db92ec3ad955ce27af
suizenji <suizenji@pop-os.localdomain>
2022-09-05 13:05:38 +0900

    wireframe

--- app/src/Controller/WireframeController.php
+++ app/src/Controller/WireframeController.php
@@ -2,25 +2,22 @@
 
 namespace App\Controller;
 
-use App\Workflow\Entity\WireframeEntity;
+use App\Workflow\Wireframe\Wireframe;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Workflow\Registry;
-use Symfony\Component\Workflow\Workflow;
 
 class WireframeController extends AbstractController
 {
-    public function __construct(private Registry $workflows)
+    public function __construct(private Wireframe $wf)
     {
     }
 
     #[Route('/wireframe', name: 'app_wireframe')]
-    public function index(WireframeEntity $wireframe): Response
+    public function index(): Response
     {
-        $stateMachine = $this->workflows->get($wireframe);
-
-        var_dump($stateMachine->can($wireframe, 'visit'));
+        $this->wf->reset('my_wireframe');
+        $this->dump();
 
         return $this->render('wireframe/index.html.twig', [
             'controller_name' => 'index',
@@ -30,6 +27,18 @@ class WireframeController extends AbstractController
     #[Route('/wireframe/input', name: 'app_wireframe_input')]
     public function input(): Response
     {
+        $this->dump();
+
+        if ($this->wf->isValid('my_wireframe', 'visit')) {
+            echo 'first access';
+            $this->wf->update('my_wireframe', 'visit');
+        } else if ($this->wf->isValid('my_wireframe', 'rewrite')) {
+            echo 'rewrite';
+            $this->wf->update('my_wireframe', 'rewrite');
+        } else {
+            echo 'NG';
+        }
+
         return $this->render('wireframe/index.html.twig', [
             'controller_name' => 'input',
         ]);
@@ -38,6 +47,15 @@ class WireframeController extends AbstractController
     #[Route('/wireframe/confirm', name: 'app_wireframe_confirm')]
     public function confirm(): Response
     {
+        $this->dump();
+
+        if ($this->wf->isValid('my_wireframe', 'write_out')) {
+            echo 'write_out';
+            $this->wf->update('my_wireframe', 'write_out');
+        } else {
+            echo 'NG';
+        }
+
         return $this->render('wireframe/index.html.twig', [
             'controller_name' => 'confirm',
         ]);
@@ -46,8 +64,36 @@ class WireframeController extends AbstractController
     #[Route('/wireframe/result', name: 'app_wireframe_result')]
     public function result(): Response
     {
+        $this->dump();
+
+        if ($this->wf->isValid('my_wireframe', 'submit')) {
+            echo 'submit';
+            $this->wf->update('my_wireframe', 'submit');
+        } else {
+            echo 'NG';
+        }
+
         return $this->render('wireframe/index.html.twig', [
             'controller_name' => 'result',
         ]);
+    }
+
+    public function dump()
+    {
+        $entity = $this->wf->getEntity('my_wireframe');
+        echo 'place:';
+        var_dump($entity->getCurrentPlace());
+
+        echo 'visit:';
+        var_dump($this->wf->isValid('my_wireframe', 'visit'));
+
+        echo 'write_out:';
+        var_dump($this->wf->isValid('my_wireframe', 'write_out'));
+
+        echo 'submit:';
+        var_dump($this->wf->isValid('my_wireframe', 'submit'));
+
+        echo 'rewrite';
+        var_dump($this->wf->isValid('my_wireframe', 'rewrite'));
     }
 }
