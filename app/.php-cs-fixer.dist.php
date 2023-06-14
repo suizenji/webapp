1d78784f6c0df7e846cf366a5ee9fa1e17bfbcae
suizenji <suizenji@DESKTOP-GRCOHLF>
2023-06-14 12:20:26 +0900

    cs-fixer-config

--- app/.php-cs-fixer.dist.php
+++ app/.php-cs-fixer.dist.php
@@ -1,16 +1,33 @@
 <?php
 
+/**
+ * @see https://mlocati.github.io/php-cs-fixer-configurator/
+ * @see https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-12-extended-coding-style-guide.md
+ */
+
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
-    ->exclude('var')
+    ->exclude([
+        'var',
+        'vendor',
+    ])
 ;
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
         '@PHP81Migration' => true,
-        'phpdoc_summary' => false,
-        'concat_space' => false,
+        'concat_space' => ['spacing' => 'one'],
+        'phpdoc_summary' => false, // .つけたくない
+        'phpdoc_to_comment' => false, // コメントにしたくない
+//        'single_line_throw' => false,
+        'ordered_imports' => [
+            'imports_order' => [ // デフォだとclassとfunctionが同列に扱われる
+                'class',
+                'function',
+                'const',
+            ],
+        ],
     ])
     ->setFinder($finder)
 ;
